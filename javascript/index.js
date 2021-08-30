88e9f36dcde3cd2fcb454d38f74a06f53bf494af
Fourcaud <remi.fourcaud@gmail.com>
2021-08-30 17:09:23 +0200

    Mise en place de tout les filtre

--- javascript/index.js
+++ javascript/index.js
@@ -6,35 +6,96 @@ function fetchData(callback) {
       callback(data.recipes);
     });
 }
+const filtreAtif = [];
 
 //fonction pour afficher toutes les recettes - appeler au debut et a chaque modification de filtre
+
 function fetchRecipes(recipes) {
   let recipeContainer = document.getElementById("container-card");
   let htmlrecipe = "";
+  if (filtreAtif.length > 0) {
+    for (let actif in filtreAtif) {
+      const result1 = recipes.filter((search) =>
+        search.name.toLowerCase().includes(filtreAtif)
+      );
 
-  if (recipes.length > 0) {
-    for (let recipe in recipes) {
-      htmlrecipe += `<div class="card">`;
-      htmlrecipe += `<div class="card__img"></div>`;
-      htmlrecipe += `<div class="card__recipe"> <h2>${recipes[recipe].name}</h2> <i class="far fa-clock"></i><h3>${recipes[recipe].time}min</h3></div>`;
-      htmlrecipe += `<div class="card__text"><div class="card__ingredients">`;
+      const result2 = recipes.filter((search) =>
+        search.appliance.toLowerCase().includes(filtreAtif)
+      );
 
-      for (let ingredientLoop in recipes[recipe].ingredients) {
-        let unitTrue = "";
-        if (recipes[recipe].ingredients[ingredientLoop].unit) {
-          unitTrue = recipes[recipe].ingredients[ingredientLoop].unit;
-        }
-        htmlrecipe += `<h4>${recipes[recipe].ingredients[ingredientLoop].ingredient} : ${recipes[recipe].ingredients[ingredientLoop].quantity} ${unitTrue} </h4>`;
-      }
-      htmlrecipe += `</div>`;
-      htmlrecipe += `<div class="card__description"><p>${recipes[recipe].description}</p></div>`;
-      htmlrecipe += `</div>`;
-      htmlrecipe += `</div>`;
+      const result3 = recipes.filter((search) =>
+        search.ingredients.some((searchArray) =>
+          searchArray.ingredient.toLowerCase().includes(filtreAtif)
+        )
+      );
+
+      const result4 = recipes.filter((search) =>
+        search.ustensils.some((searchArray) =>
+          searchArray.toLowerCase().includes(filtreAtif)
+        )
+      );
     }
-    recipeContainer.innerHTML = htmlrecipe;
+
+    function removeDuplicates(inArray) {
+      let arr = inArray.concat();
+
+      for (let i = 0; i < arr.length; ++i) {
+        for (let j = i + 1; j < arr.length; ++j) {
+          if (arr[i] === arr[j]) {
+            arr.splice(j, 1);
+          }
+        }
+      }
+      return arr;
+    }
+    const resultAll = result1.concat(result2, result3, result4);
+    const result = removeDuplicates(resultAll);
+    if (result.length > 0) {
+      for (let recipe in result) {
+        htmlrecipe += `<div class="card">`;
+        htmlrecipe += `<div class="card__img"></div>`;
+        htmlrecipe += `<div class="card__recipe"> <h2>${result[recipe].name}</h2> <i class="far fa-clock"></i><h3>${result[recipe].time}min</h3></div>`;
+        htmlrecipe += `<div class="card__text"><div class="card__ingredients">`;
+
+        for (let ingredientLoop in result[recipe].ingredients) {
+          let unitTrue = "";
+          if (result[recipe].ingredients[ingredientLoop].unit) {
+            unitTrue = result[recipe].ingredients[ingredientLoop].unit;
+          }
+          htmlrecipe += `<h4>${result[recipe].ingredients[ingredientLoop].ingredient} : ${result[recipe].ingredients[ingredientLoop].quantity} ${unitTrue} </h4>`;
+        }
+        htmlrecipe += `</div>`;
+        htmlrecipe += `<div class="card__description"><p>${result[recipe].description}</p></div>`;
+        htmlrecipe += `</div>`;
+        htmlrecipe += `</div>`;
+      }
+      recipeContainer.innerHTML = htmlrecipe;
+    }
   } else {
-    htmlrecipe += `<div class="error-Search">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</div>`;
-    recipeContainer.innerHTML = htmlrecipe;
+    if (recipes.length > 0) {
+      for (let recipe in recipes) {
+        htmlrecipe += `<div class="card">`;
+        htmlrecipe += `<div class="card__img"></div>`;
+        htmlrecipe += `<div class="card__recipe"> <h2>${recipes[recipe].name}</h2> <i class="far fa-clock"></i><h3>${recipes[recipe].time}min</h3></div>`;
+        htmlrecipe += `<div class="card__text"><div class="card__ingredients">`;
+
+        for (let ingredientLoop in recipes[recipe].ingredients) {
+          let unitTrue = "";
+          if (recipes[recipe].ingredients[ingredientLoop].unit) {
+            unitTrue = recipes[recipe].ingredients[ingredientLoop].unit;
+          }
+          htmlrecipe += `<h4>${recipes[recipe].ingredients[ingredientLoop].ingredient} : ${recipes[recipe].ingredients[ingredientLoop].quantity} ${unitTrue} </h4>`;
+        }
+        htmlrecipe += `</div>`;
+        htmlrecipe += `<div class="card__description"><p>${recipes[recipe].description}</p></div>`;
+        htmlrecipe += `</div>`;
+        htmlrecipe += `</div>`;
+      }
+      recipeContainer.innerHTML = htmlrecipe;
+    } else {
+      htmlrecipe += `<div class="error-Search">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</div>`;
+      recipeContainer.innerHTML = htmlrecipe;
+    }
   }
 }
 
@@ -49,19 +110,23 @@ function showTriIngredient(recipes) {
 
   for (let i in recipes) {
     for (let j in recipes[i].ingredients) {
-      for (let k in recipes[i].ingredients[j].ingredient)
+      if (
+        recipes[i].ingredients[j].ingredient
+          .toLowerCase()
+          .includes(recipes[recipes.length - 1].search) ||
+        !recipes[recipes.length - 1].search
+      ) {
         allIngredient.push(recipes[i].ingredients[j].ingredient);
+      }
     }
+    let uniqueTypes = allIngredient.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+    for (let l in uniqueTypes) {
+      htmlnavFiltre += `<li onclick='clickIngredient("${uniqueTypes[l]}")'><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags" value="${uniqueTypes[l]}"><label class="labeltags">${uniqueTypes[l]}</label></li>`;
+    }
+    navFiltre.innerHTML = htmlnavFiltre;
   }
-  let uniqueTypes = allIngredient.filter(
-    (value, index, self) => self.indexOf(value) === index
-  );
-
-  for (let l in uniqueTypes) {
-    htmlnavFiltre += `<li><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags" value="${uniqueTypes[l]}"><label class="labeltags">${uniqueTypes[l]}</label></li>`;
-  }
-
-  navFiltre.innerHTML = htmlnavFiltre;
 }
 
 function myFunctionTriIngredient() {
@@ -99,10 +164,27 @@ function ingredientFilter(recipes) {
         searchArray.ingredient.toLowerCase().includes(element)
       )
     );
+    result.push({ search: element });
     showTriIngredient(result);
   });
 }
 window.ingredientFilter = ingredientFilter;
+function clickIngredient(x) {
+  filtreAtif.push(x);
+  let uniqueTypes = filtreAtif.filter(
+    (value, index, self) => self.indexOf(value) === index
+  );
+  showLabelIngredient(uniqueTypes);
+}
+window.clickIngredient = clickIngredient;
+
+function showLabelIngredient(name) {
+  let labelIngredient = document.getElementById("container-label");
+  let htmllabelIngredient = "";
+  htmllabelIngredient += `<div>${name}</div>`;
+  labelIngredient.innerHTML = htmllabelIngredient;
+}
+window.showLabelIngredient = showLabelIngredient;
 
 /* ---------------------------------------------------------------------------- */
 
@@ -122,7 +204,7 @@ function showTriDevice(recipes) {
   );
 
   for (let l in uniqueTypes) {
-    htmlnavFiltre += `<li><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags" ><label class="labeltags">${uniqueTypes[l]}</label></li>`;
+    htmlnavFiltre += `<li onclick='clickDevice("${uniqueTypes[l]}")'><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags" ><label class="labeltags">${uniqueTypes[l]}</label></li>`;
   }
 
   navFiltre.innerHTML = htmlnavFiltre;
@@ -164,6 +246,22 @@ function deviceFilter(recipes) {
 }
 window.deviceFilter = deviceFilter;
 
+function clickDevice(x) {
+  filtreAtif.push(x);
+  let uniqueTypes = filtreAtif.filter(
+    (value, index, self) => self.indexOf(value) === index
+  );
+  showLabelDevice(uniqueTypes);
+}
+window.clickDevice = clickDevice;
+
+function showLabelDevice(name) {
+  let labelDevice = document.getElementById("container-label");
+  let htmllabelDevice = "";
+  htmllabelDevice += `<div>${name}</div>`;
+  labelDevice.innerHTML = htmllabelDevice;
+}
+window.showLabelDevice = showLabelDevice;
 /* ---------------------------------------------------------------------------- */
 
 //Groupe de fonction qui permet d'afficher les ustensils dans le bouton
@@ -174,17 +272,22 @@ function showTriUtensils(recipes) {
   let allUtensils = [];
   for (let i in recipes) {
     for (let j in recipes[i].ustensils) {
-      allUtensils.push(recipes[i].ustensils);
+      if (
+        recipes[i].ustensils[j]
+          .toLowerCase()
+          .includes(recipes[recipes.length - 1].search) ||
+        !recipes[recipes.length - 1].search
+      ) {
+        allUtensils.push(recipes[i].ustensils[j]);
+      }
     }
   }
   let uniqueTypes = allUtensils.filter(
     (value, index, self) => self.indexOf(value) === index
   );
-
   for (let l in uniqueTypes) {
-    htmlnavFiltre += `<li><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags"><label class="labeltags">${uniqueTypes[l]}</label></li>`;
+    htmlnavFiltre += `<li onclick='clickUtensil("${uniqueTypes[l]}")'><input  type="checkbox" class="check" id="${uniqueTypes[l]}" name="header__tags"><label class="labeltags">${uniqueTypes[l]}</label></li>`;
   }
-
   navFiltre.innerHTML = htmlnavFiltre;
 }
 
@@ -212,7 +315,6 @@ function myFunctionTriUtensils() {
     .getElementById("myDropdown-utensils")
     .classList.toggle("show-utensils");
 }
-
 window.myFunctionTriUtensils = myFunctionTriUtensils;
 
 const searchUt = document.getElementById("searchUtensils");
@@ -224,10 +326,30 @@ function utensilsFilter(recipes) {
         searchArray.toLowerCase().includes(element)
       )
     );
+    result.push({ search: element });
     showTriUtensils(result);
   });
 }
 window.utensilsFilter = utensilsFilter;
+
+function clickUtensil(x) {
+  filtreAtif.push(x);
+  let uniqueTypes = filtreAtif.filter(
+    (value, index, self) => self.indexOf(value) === index
+  );
+  showLabelUtensil(uniqueTypes);
+
+  fetchRecipes(recipes);
+}
+window.clickUtensil = clickUtensil;
+
+function showLabelUtensil(name) {
+  let labelUtensil = document.getElementById("container-label");
+  let htmllabelUtensil = "";
+  htmllabelUtensil += `<div>${name}</div>`;
+  labelUtensil.innerHTML = htmllabelUtensil;
+}
+window.showLabelUtensil = showLabelUtensil;
 
 /* ---------------------------------------------------------------------------- */
 
